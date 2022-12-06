9f1ff01befa70b0bda00d92bc110c1914a89ce6d
Antonio Salvati <antonio.salvati@skillbill.it>
2022-12-06 08:55:38 +0100

    day 06 - cleanup

--- 2022/06/puzzle.js
+++ 2022/06/puzzle.js
@@ -1,10 +1,10 @@
-import { readInput, sum } from "../utils.js";
+import { readInput } from "../utils.js";
 
-const { values: { 0: stream } } = (await readInput({ sourceUrl: import.meta.url, parser: l => l.split('') }));
+const { values: { 0: stream } } = await readInput({ sourceUrl: import.meta.url, parser: l => l.split('') });
 
 // part 1
 
-const findMarker = n => stream.findIndex((_, i, a) => new Set(new Array(n).fill(null).map((_, x) => a[i + x])).size === n) + n;
+const findMarker = n => n + stream.findIndex((_, i, a) => new Set(new Array(n).fill().map((_, x) => a[i + x])).size === n);
 
 console.log(findMarker(4));
 
