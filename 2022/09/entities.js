8f49a3b5a5e14a6366b64fa3b7caed7d9357dd5b
Antonio Salvati <antonio.salvati@skillbill.it>
2022-12-09 10:30:04 +0100

    day 09

---
+++ 2022/09/entities.js
@@ -0,0 +1,53 @@
+export class Point {
+  x = 0;
+  y = 0;
+
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  toString() {
+    return `x:${this.x},y:${this.y}`;
+  }
+}
+
+export class Knot {
+  positions = [new Point(0, 0)];
+
+  move(dir) {
+
+    const pos = this.positions[this.positions.length - 1];
+
+    switch (dir) {
+      case "U":
+        this.positions.push(new Point(pos.x, pos.y - 1));
+        break;
+      case "D":
+        this.positions.push(new Point(pos.x, pos.y + 1));
+        break;
+      case "L":
+        this.positions.push(new Point(pos.x - 1, pos.y));
+        break;
+      case "R":
+        this.positions.push(new Point(pos.x + 1, pos.y));
+        break;
+    }
+  }
+
+  follow(other) {
+    const myPos = this.positions[this.positions.length - 1];
+    const otherPos = other.positions[other.positions.length - 1];
+
+    const diffX = otherPos.x - myPos.x;
+    const diffY = otherPos.y - myPos.y;
+
+    if (Math.abs(diffX) > 1 || Math.abs(diffY) > 1) {
+      this.positions.push(new Point(myPos.x + Math.sign(diffX), myPos.y + Math.sign(diffY)));
+    }
+  }
+
+  uniquePositions() {
+    return new Set(this.positions.map(p => p.toString())).size;
+  }
+}
---
+++ 2022/09/puzzle.js
@@ -0,0 +1,46 @@
+import { readInput } from "../utils.js";
+import { Knot } from "./entities.js";
+
+const parser = line => {
+  const parts = line.split(' ');
+  return {
+    dir: parts[0],
+    steps: parseInt(parts[1], 10)
+  }
+};
+
+const { values: moves } = await readInput({ sourceUrl: import.meta.url, parser });
+
+// part 1
+
+const head = new Knot();
+const tail = new Knot();
+
+for (let i = 0; i < moves.length; i++) {
+  const move = moves[i];
+  let steps = move.steps;
+  while (steps--) {
+    head.move(move.dir);
+    tail.follow(head);
+  }
+}
+
+console.log(tail.uniquePositions());
+
+// part 2
+
+const rope = new Array(10).fill().map(() => new Knot());
+
+for (let i = 0; i < moves.length; i++) {
+  const move = moves[i];
+  let steps = move.steps;
+  while (steps--) {
+    rope[0].move(move.dir);
+    for (let x = 1; x < rope.length; x++) {
+      rope[x].follow(rope[x - 1]);
+    }
+    tail.follow(head);
+  }
+}
+
+console.log(rope[rope.length - 1].uniquePositions());
