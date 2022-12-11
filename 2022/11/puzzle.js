3523417bedb318fcf847553447dd3a504064dc77
Antonio Salvati <salvan13@gmail.com>
2022-12-11 14:18:17 +0100

    day 11 - part 2

--- 2022/11/puzzle.js
+++ 2022/11/puzzle.js
@@ -8,6 +8,8 @@ const { values } = await readInput({ sourceUrl: import.meta.url, parser });
 // part 1
 
 const round = (monkeys, cfg) => {
+  const mod = cfg.mod && cfg.mod(monkeys);
+
   for (let m = 0; m < monkeys.length; m++) {
     const monkey = monkeys[m];
     while (monkey.items.length) {
@@ -17,21 +19,21 @@ const round = (monkeys, cfg) => {
 
       const ctx = {
         old: item,
-        result: null,
-        test: monkey.test,
-        divide: cfg.divide,
         newVal: null
       };
 
-      vm.runInNewContext(`
-        ${monkey.op};
-        if (divide) {
-          newVal = Math.floor(newVal / divide);
-        }
-        result = newVal % test === 0;
-      `, ctx);
+      vm.runInNewContext(monkey.op, ctx);
 
-      monkeys[monkey[ctx.result ? "nextTrue" : "nextFalse"]].items.push(ctx.newVal);
+      let newVal = ctx.newVal;
+
+      if (cfg.divide) {
+        newVal = Math.floor(newVal / cfg.divide);
+      } else if (mod) {
+        newVal %= mod;
+      }
+
+      const nextMonkey = monkeys[monkey[newVal % monkey.test === 0 ? "nextTrue" : "nextFalse"]];
+      nextMonkey.items.push(newVal);
     }
   }
 }
@@ -39,6 +41,7 @@ const round = (monkeys, cfg) => {
 const run = (times, cfg = {}) => {
   const monkeys = createMonkeyList(values);
   Array.from({ length: times }).forEach((_, i) => {
+    i % 500 === 0 ? console.log("step", i) : null;
     round(monkeys, cfg);
   });
   monkeys.sort((m1, m2) => m2.inspects - m1.inspects);
@@ -49,4 +52,4 @@ console.log(run(20, { divide: 3 }));
 
 // part 2
 
-//console.log(run(10_000));
+console.log(run(10_000, { mod: monkeys => monkeys.map(m => m.test).reduce((a, c) => a * c, 1) }));
