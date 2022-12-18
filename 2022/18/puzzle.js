862a0a178b9e800596d214b3adfd9a6dd446e55c
Antonio Salvati <salvan13@gmail.com>
2022-12-18 11:23:54 +0100

    day 18 - part 2

--- 2022/18/puzzle.js
+++ 2022/18/puzzle.js
@@ -45,3 +45,75 @@ const countConnecteds = () => {
 const externalSurface = cubes.length * 6 - countConnecteds() * 2;
 
 console.log(externalSurface);
+
+// part 2
+
+const minX = Math.min(...cubes.map(c => c.x)) - 1;
+const minY = Math.min(...cubes.map(c => c.y)) - 1;
+const minZ = Math.min(...cubes.map(c => c.z)) - 1;
+const maxX = Math.max(...cubes.map(c => c.x)) + 1;
+const maxY = Math.max(...cubes.map(c => c.y)) + 1;
+const maxZ = Math.max(...cubes.map(c => c.z)) + 1;
+
+const getAllPositions = () => {
+  const positions = [];
+
+  for (let x = minX; x <= maxX; x++) {
+    for (let y = minY; y <= maxY; y++) {
+      for (let z = minZ; z <= maxZ; z++) {
+        const cube = cubes.find(c => c.x === x && c.y === y && c.z === z);
+        if (cube) {
+          positions.push({ ...cube, type: "solid" });
+        } else {
+          positions.push({ x, y, z, type: "air" });
+        }
+      }
+    }
+  }
+
+  return positions;
+};
+
+const positions = getAllPositions();
+
+const getAdjacentPositions = p => {
+  const ps = [];
+  for (let i = 0; i < positions.length; i++) {
+    if (connecteds(p, positions[i])) {
+      ps.push(positions[i]);
+    }
+  }
+  return ps;
+};
+
+const markExternalPositions = c => {
+  const adjs = getAdjacentPositions(c);
+
+  if (c.type === "air" && (adjs.length < 6 || adjs.find(a => a.type === "external"))) {
+    c.type = "external";
+  }
+
+  for (let i = 0; i < adjs.length; i++) {
+    if (adjs[i].type === "air") {
+      markExternalPositions(adjs[i]);
+    }
+  }
+};
+
+markExternalPositions(positions.find(p => p.x === minX && p.y === minY && p.z === minZ));
+
+const countExternalSides = () => {
+  let n = 0
+
+  for (let x = 0; x < cubes.length; x++) {
+    const cube = cubes[x];
+    const adjs = getAdjacentPositions(cube);
+    n += adjs.filter(p => p.type === "external").length;
+  }
+
+  return n;
+}
+
+console.log(countExternalSides());
+
+// run node with the option --stack-size=65500 in part 2
