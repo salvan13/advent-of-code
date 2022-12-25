d6b59aa4d973accf8f39d92c242d978e51ebcb80
Antonio Salvati <salvan13@gmail.com>
2022-12-25 11:12:46 +0100

    day 22 - part 2

--- 2022/22/puzzle.js
+++ 2022/22/puzzle.js
@@ -13,7 +13,7 @@ const path = values.at(-1).match(/(\d+)([L|R]?)/g).map(s => ({
 
 const DIRS = ["R", "D", "L", "U"];
 
-const run = () => {
+const run = (findNextCell) => {
   let pos = { y: 0, x: map[0].findIndex(c => c === ".") };
   let dir = "R";
   const history = [];
@@ -24,29 +24,12 @@ const run = () => {
   };
 
   const step = () => {
-    const next = { ...pos };
-    let nextCell = null;
-
-    while (!nextCell) {
-      switch (dir) {
-        case "L":
-          next.x = (next.x - 1) < 0 ? map[next.y].length - 1 : next.x - 1;
-          break;
-        case "R":
-          next.x = (next.x + 1) > map[next.y].length - 1 ? 0 : next.x + 1;
-          break;
-        case "U":
-          next.y = (next.y - 1) < 0 ? map.length - 1 : next.y - 1;
-          break;
-        case "D":
-          next.y = (next.y + 1) > map.length - 1 ? 0 : next.y + 1;
-          break;
-      }
-      nextCell = map.at(next.y).at(next.x);
-    }
+    const { nextPos, nextDir } = findNextCell({ pos, dir });
+    const nextCell = map[nextPos.y][nextPos.x];
 
     if (nextCell === ".") {
-      pos = next;
+      pos = nextPos;
+      dir = nextDir;
       history.push({ pos, dir });
       return true;
     }
@@ -67,12 +50,133 @@ const run = () => {
   return { pos, dir };
 };
 
-const printMap = (map, history) => {
-  const m = map.map((row, y) => row.map((cell, x) => cell || " "));
-  history.forEach(h => m[h.pos.y][h.pos.x] = h.dir);
+const printMap = (map, positions) => {
+  const m = map.map(row => row.map(cell => cell || " "));
+  positions.forEach(h => m[h.pos.y][h.pos.x] = h.dir);
   console.log(m.map(r => r.join("")).join("\n"));
 };
 
-const { pos, dir } = run();
-console.log(pos, dir)
-console.log((pos.y + 1) * 1000 + (pos.x + 1) * 4 + DIRS.indexOf(dir))
+const findNextCell = ({ pos, dir }) => {
+  const next = { ...pos };
+  let nextCell = null;
+
+  while (!nextCell) {
+    switch (dir) {
+      case "L":
+        next.x = (next.x - 1) < 0 ? map[next.y].length - 1 : next.x - 1;
+        break;
+      case "R":
+        next.x = (next.x + 1) > map[next.y].length - 1 ? 0 : next.x + 1;
+        break;
+      case "U":
+        next.y = (next.y - 1) < 0 ? map.length - 1 : next.y - 1;
+        break;
+      case "D":
+        next.y = (next.y + 1) > map.length - 1 ? 0 : next.y + 1;
+        break;
+    }
+    nextCell = map[next.y][next.x];
+  }
+
+  return {
+    nextPos: next,
+    nextDir: dir
+  };
+};
+
+{
+  const { pos, dir } = run(findNextCell);
+  console.log((pos.y + 1) * 1000 + (pos.x + 1) * 4 + DIRS.indexOf(dir))
+}
+
+// part 2
+
+const portals = {};
+
+const range = (start, end) => {
+  if (start < end) {
+    return new Array(end - start + 1).fill().map((_, i) => i + start);
+  } else {
+    return new Array(start - end + 1).fill().map((_, i) => start - i);
+  }
+}
+
+const addPortals = (a, b, dirA, dirB) => {
+  const pa = [];
+  const pb = [];
+  for (const y of range(...a.y)) {
+    for (const x of range(...a.x)) {
+      pa.push({ y, x })
+    }
+  }
+  for (const y of range(...b.y)) {
+    for (const x of range(...b.x)) {
+      pb.push({ y, x })
+    }
+  }
+  if (pa.length !== pb.length) {
+    throw new Error("oops")
+  }
+  for (let i = 0; i < pa.length; i++) {
+    portals[`${pa[i].y}:${pa[i].x}:${dirA}`] = { pos: pb[i], dir: dirB };
+  }
+};
+
+// test portals
+// addPortals({ y: [4, 7], x: [12, 12] }, { y: [8, 8], x: [15, 12] }, "R", "D");
+// addPortals({ y: [12, 12], x: [8, 11] }, { y: [7, 7], x: [3, 0] }, "D", "U");
+// addPortals({ y: [3, 3], x: [4, 7] }, { y: [0, 3], x: [8, 8] }, "U", "R");
+
+// input portals
+addPortals({ y: [-1, -1], x: [50, 99] }, { y: [150, 199], x: [0, 0] }, "U", "R");
+addPortals({ y: [-1, -1], x: [100, 149] }, { y: [199, 199], x: [0, 49] }, "U", "U");
+addPortals({ y: [0, 49], x: [49, 49] }, { y: [149, 100], x: [0, 0] }, "L", "R");
+addPortals({ y: [0, 49], x: [150, 150] }, { y: [149, 100], x: [99, 99] }, "R", "L");
+addPortals({ y: [50, 50], x: [100, 149] }, { y: [50, 99], x: [99, 99] }, "D", "L");
+addPortals({ y: [50, 99], x: [100, 100] }, { y: [49, 49], x: [100, 149] }, "R", "U");
+addPortals({ y: [50, 99], x: [49, 49] }, { y: [100, 100], x: [0, 49] }, "L", "D");
+addPortals({ y: [99, 99], x: [0, 49] }, { y: [50, 99], x: [50, 50] }, "U", "R");
+addPortals({ y: [100, 149], x: [100, 100] }, { y: [49, 0], x: [149, 149] }, "R", "L");
+addPortals({ y: [150, 150], x: [50, 99] }, { y: [150, 199], x: [49, 49] }, "D", "L");
+addPortals({ y: [100, 149], x: [-1, -1] }, { y: [49, 0], x: [50, 50] }, "L", "R");
+addPortals({ y: [200, 200], x: [0, 49] }, { y: [0, 0], x: [100, 149] }, "D", "D");
+addPortals({ y: [150, 199], x: [-1, -1] }, { y: [0, 0], x: [50, 99] }, "L", "D");
+addPortals({ y: [150, 199], x: [50, 50] }, { y: [149, 149], x: [50, 99] }, "R", "U");
+
+const findNextCubeCell = ({ pos, dir }) => {
+  const next = { ...pos };
+
+  switch (dir) {
+    case "L":
+      next.x--;
+      break;
+    case "R":
+      next.x++;
+      break;
+    case "U":
+      next.y--;
+      break;
+    case "D":
+      next.y++;
+      break;
+  }
+
+  const nextCell = map[next.y] && map[next.y][next.x];
+
+  if (!nextCell) {
+    const p = portals[`${next.y}:${next.x}:${dir}`];
+    next.x = p.pos.x;
+    next.y = p.pos.y;
+    dir = p.dir;
+  }
+
+  return {
+    nextPos: next,
+    nextDir: dir
+  };
+};
+
+{
+  const { pos, dir } = run(findNextCubeCell);
+  console.log((pos.y + 1) * 1000 + (pos.x + 1) * 4 + DIRS.indexOf(dir))
+}
